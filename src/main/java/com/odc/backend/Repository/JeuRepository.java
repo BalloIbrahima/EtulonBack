3bb9af0c4036cb2bb3db2c7e17402f14bb1d0b15
BalloIbrahima <ibrahimaballo01@gmail.com>
2022-12-30 21:58:30 +0000

    remove repository anotation because its unassessary

--- src/main/java/com/odc/backend/Repository/JeuRepository.java
+++ src/main/java/com/odc/backend/Repository/JeuRepository.java
@@ -1,10 +1,8 @@
 package com.odc.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.odc.backend.Models.Jeu;
-@Repository
 public interface JeuRepository  extends JpaRepository<Jeu, Long>{
     
     Jeu findByNom(String nom);
