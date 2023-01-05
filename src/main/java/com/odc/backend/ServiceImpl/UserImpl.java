77aadae013c84897d2e4a30ddb7e72cf216bda0a
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-01-05 11:57:46 +0000

    remove class avertissement

--- src/main/java/com/odc/backend/ServiceImpl/UserImpl.java
+++ src/main/java/com/odc/backend/ServiceImpl/UserImpl.java
@@ -1,8 +1,6 @@
 package com.odc.backend.ServiceImpl;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
